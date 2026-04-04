b160d22571476e8c7d169f093dafc89cbd96f94b
IluxaMS <ilya.kkaazarin@gmail.com>
2026-04-04 14:30:22 +0200

    Delete 1 Module directory

--- 1 Module/2 task/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-    if (typeof air_temperature[i] !== 'number' || air_temperature[i] === null) { // фильтрую значения
-        air_temperature[i] = 0;
-        console.log("Был исправлен индекс:", i);
-    }
