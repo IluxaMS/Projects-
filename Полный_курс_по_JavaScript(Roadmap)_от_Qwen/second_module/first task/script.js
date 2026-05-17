4a43e92a813b98846709fb53e3b3ce986fc77243
IluxaMS <ilya.kkaazarin@gmail.com>
2026-05-17 13:39:00 +0300

    Delete Полный_курс_по_JavaScript(Roadmap)_от_Qwen directory

--- Полный_курс_по_JavaScript(Roadmap)_от_Qwen/second_module/first task/script.js
+++
@@ -1,3 +0,0 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const sorting_parity = (array = []) => array.filter(x => x % 2 === 0);
-console.log(sorting_parity(list));
--- Полный_курс_по_JavaScript(Roadmap)_от_Qwen/second_module/third task/script.js
+++
@@ -1,3 +0,0 @@
-const validator = (text) => text.includes("@") && text.includes(".");
-console.log(validator("test@mail.ru")); // true
-console.log(validator("test@mailru")); // false
